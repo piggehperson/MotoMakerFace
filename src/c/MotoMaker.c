14aa6c94ba3c434c4fbd952e74ed77e5c834a391
Lavender Glaab <piggehperson@gmail.com>
2020-03-24 17:09:51 -0400

    Add seconds hand and center dot to watch face

--- src/c/MotoMaker.c
+++ src/c/MotoMaker.c
@@ -1,6 +1,7 @@
 #include <pebble.h>
 
 static Window *s_main_window;
+
 static Layer *s_background_layer;
 static Layer *s_hands_layer;
 static Layer *s_seconds_layer;
@@ -47,7 +48,7 @@ static void hands_layer_update_proc(Layer *layer, GContext *ctx) {
   //graphics_context_set_fill_color(ctx, GColorClear);
   //graphics_fill_rect(ctx, bounds, 0, GCornerNone);
 
-  const int16_t max_hand_length = (bounds.size.w -10) / 2;
+  const int16_t max_hand_length = PBL_IF_ROUND_ELSE(((bounds.size.w - 30) / 2), (bounds.size.w - 10) / 2);
   
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
@@ -55,16 +56,47 @@ static void hands_layer_update_proc(Layer *layer, GContext *ctx) {
   int32_t minute_angle = TRIG_MAX_ANGLE * t->tm_min / 60;
 
   // draw minute hand 
-  draw_hand(ctx, center, minute_angle, max_hand_length, 6, GColorWhite);
+  draw_hand(ctx, center, minute_angle, max_hand_length, 5, GColorWhite);
   
   
   // calculate hour hand
   int32_t hour_angle = (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6);
   
   // draw shadow for hour hand, so it doesnt get obscured
-  draw_hand(ctx, center, hour_angle, max_hand_length * 0.6, 8, GColorBlack);
+  draw_hand(ctx, center, hour_angle, max_hand_length * 0.6, 7, GColorBlack);
   // draw hour hand
-  draw_hand(ctx, center, hour_angle, max_hand_length * 0.6, 6, GColorWhite);
+  draw_hand(ctx, center, hour_angle, max_hand_length * 0.6, 5, GColorWhite);
+
+  
+  // Draw a shadow around the center dot
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_circle(ctx, center, 7);
+
+  // Draw the center dot
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_circle(ctx, center, 3);
+}
+
+static void seconds_layer_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+  GPoint center = grect_center_point(&bounds);
+
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+  // calculate hand
+  int32_t angle = TRIG_MAX_ANGLE * t->tm_sec / 60;
+
+  // draw hand 
+  draw_hand(ctx, center, angle, bounds.size.h, 2, PBL_IF_COLOR_ELSE(GColorRed, GColorWhite));
+  
+  
+  // Draw a shadow around the center dot
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_circle(ctx, center, 7);
+
+  // Draw the center dot
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_circle(ctx, center, 3);
 }
 
 static void background_layer_update_proc(Layer *layer, GContext *ctx) {
@@ -73,7 +105,7 @@ static void background_layer_update_proc(Layer *layer, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorBlack);
   graphics_fill_rect(ctx, bounds, 0, GCornerNone);
 
-  // add ticks, logo, and stuff
+  // add ticks
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -88,13 +120,29 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   // Update seconds hand if enabled
   if (enable_seconds) {
     APP_LOG(APP_LOG_LEVEL_DEBUG, "mark seconds layer dirty");
-    //layer_mark_dirty(s_seconds_layer);
+    layer_mark_dirty(s_seconds_layer);
   }
 }
 
-static void initialize_seconds(
-  
-)
+static void initialize_seconds() {
+  tick_timer_service_unsubscribe();
+
+  if (enable_seconds) {
+    // Register with TickTimerService
+    tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Subscribed to tick handler for seconds");
+
+    // Show seconds layer
+    layer_set_hidden(s_seconds_layer, false);
+  } else {
+    // Register with TickTimerService
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Subscribed to tick handler for minutes");
+
+    // Hide seconds layer
+    layer_set_hidden(s_seconds_layer, true);
+  }
+}
 
 static void main_window_load(Window *window) {
   // Get information about the Window
@@ -113,14 +161,19 @@ static void main_window_load(Window *window) {
   layer_set_update_proc(s_hands_layer, hands_layer_update_proc);
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Hands layer added");
 
-  
+  // Create seconds layer
+  s_seconds_layer = layer_create(bounds);
+  layer_set_update_proc(s_seconds_layer, seconds_layer_update_proc);
+  layer_add_child(root_layer, s_seconds_layer);
+  // Show or hide seconds layer, and
+  // subscribe to correct tick timer service
+  initialize_seconds();
 }
 
 static void main_window_unload(Window *window) {
   layer_destroy(s_background_layer);
   layer_destroy(s_hands_layer);
   layer_destroy(s_seconds_layer);
-
 }
 
 static void init() {
@@ -133,10 +186,6 @@ static void init() {
     .unload = main_window_unload
   });
 
-  // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Subscribed to tick handler");
-
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
 }
@@ -144,6 +193,7 @@ static void init() {
 static void deinit() {
   // Destroy Window
   window_destroy(s_main_window);
+  tick_timer_service_unsubscribe();
 }
 
 int main(void) {
