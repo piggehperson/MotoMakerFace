ccfdd280de3d5c990cc405f5df3fd7682c8bd141
Lavender Glaab <piggehperson@gmail.com>
2020-04-19 19:40:25 -0400

    Draw hands a specified inset from center instead of cropping the inside, makes antialiasing look nice

--- src/c/MotoMaker.c
+++ src/c/MotoMaker.c
@@ -164,17 +164,28 @@ static void draw_hand(
   GContext *ctx,
   GPoint center,
   int angle,
+  int inset,
   int length,
   int thickness,
   GColor color
 ) {
+
+  // Calculate where the start point of the hand goes
+  GPoint hand_start = {
+    .x = (int16_t)(sin_lookup(angle) * (int32_t)inset / TRIG_MAX_RATIO) + center.x,
+    .y = (int16_t)(-cos_lookup(angle) * (int32_t)inset / TRIG_MAX_RATIO) + center.y,
+  };
+
   // Calculate where the end point of the hand goes
   GPoint hand_end = {
     .x = (int16_t)(sin_lookup(angle) * (int32_t)length / TRIG_MAX_RATIO) + center.x,
     .y = (int16_t)(-cos_lookup(angle) * (int32_t)length / TRIG_MAX_RATIO) + center.y,
   };
 
-  draw_line(ctx, center, hand_end, thickness, color);
+  // Draw a shadow around the hand
+  draw_line(ctx, hand_start, hand_end, thickness + 2, settings.color_background);
+  // Draw the actual hand
+  draw_line(ctx, hand_start, hand_end, thickness, color);
 }
 
 static void hands_layer_update_proc(Layer *layer, GContext *ctx) {
@@ -188,25 +199,15 @@ static void hands_layer_update_proc(Layer *layer, GContext *ctx) {
   // calculate minute hand
   int32_t minute_angle = TRIG_MAX_ANGLE * t->tm_min / 60;
 
-  // draw shadow for minute hand
-  draw_hand(ctx, center, minute_angle, max_hand_length, 7, settings.color_background);
-
   // draw minute hand
-  draw_hand(ctx, center, minute_angle, max_hand_length, 5, settings.color_minute_hand);
+  draw_hand(ctx, center, minute_angle, 10, max_hand_length, 5, settings.color_minute_hand);
 
 
   // calculate hour hand
   int32_t hour_angle = (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6);
 
-  // draw shadow for hour hand, so it doesnt get obscured
-  draw_hand(ctx, center, hour_angle, max_hand_length * 0.6, 7, settings.color_background);
   // draw hour hand
-  draw_hand(ctx, center, hour_angle, max_hand_length * 0.6, 5, settings.color_hour_hand);
-
-
-  // Draw a shadow around the center dot
-  graphics_context_set_fill_color(ctx, settings.color_background);
-  graphics_fill_circle(ctx, center, 7);
+  draw_hand(ctx, center, hour_angle, 10, max_hand_length * 0.6, 5, settings.color_hour_hand);
 
   // Draw the center dot
   graphics_context_set_fill_color(ctx, settings.color_dot);
@@ -223,12 +224,7 @@ static void seconds_layer_update_proc(Layer *layer, GContext *ctx) {
   int32_t angle = TRIG_MAX_ANGLE * t->tm_sec / 60;
 
   // draw hand
-  draw_hand(ctx, center, angle, bounds.size.h, 2, settings.color_second_hand);
-
-
-  // Draw a shadow around the center dot
-  graphics_context_set_fill_color(ctx, settings.color_background);
-  graphics_fill_circle(ctx, center, 7);
+  draw_hand(ctx, center, angle, 10, bounds.size.h, 2, settings.color_second_hand);
 
   // Draw the center dot
   graphics_context_set_fill_color(ctx, settings.color_dot);
@@ -250,19 +246,15 @@ static void background_layer_update_proc(Layer *layer, GContext *ctx) {
     int32_t angle = TRIG_MAX_ANGLE * i / 60;
 
     // draw hand
-    draw_hand(ctx, center, angle, bounds.size.h, 1, settings.color_minute_markers);
+    draw_hand(ctx, center, angle, max_hand_length * 0.8, bounds.size.h, 1, settings.color_minute_markers);
   }
   for (int i = 0; i < 60; i = i + 15) {
     // calculate hand
     int32_t angle = TRIG_MAX_ANGLE * i / 60;
 
     // draw hand
-    draw_hand(ctx, center, angle, bounds.size.h, 2, settings.color_hour_markers);
+    draw_hand(ctx, center, angle, max_hand_length * 0.8, bounds.size.h, 2, settings.color_hour_markers);
   }
-
-  // Draw a shadow around the center dot
-  graphics_context_set_fill_color(ctx, settings.color_background);
-  graphics_fill_circle(ctx, center, max_hand_length * 0.8);
 
   if (layer_get_bounds(layer).size.h == layer_get_unobstructed_bounds(layer).size.h) {
     // Draw pebble logo textLogo
